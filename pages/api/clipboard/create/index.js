543a39d3669d91016384dad1675e3fff282ea9e8
Manish Chhetri <chhetridon123@gmail.com>
2024-10-30 16:23:28 +0545

    added pusher instead of socket

--- pages/api/clipboard/create/index.js
+++ pages/api/clipboard/create/index.js
@@ -3,6 +3,15 @@ import { authOptions } from "../../auth/[...nextauth]";
 import dbConnect from "@/lib/mongodb";
 import Clipboard from "@/models/Clipboard";
 import bcrypt from "bcryptjs";
+import Pusher from "pusher";
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+  useTLS: true,
+});
 
 export default async function handler(req, res) {
   try {
@@ -36,8 +45,11 @@ export default async function handler(req, res) {
 
     // Create clipboard entry
     const clipboard = await Clipboard.create(clipboardData);
-    let io = res.socket.server.io;
-    io.to(session.user.id).emit("clipboard-update", clipboard);
+    await pusher.trigger(
+      `private-user-${session.user.id}`,
+      "clipboard-update",
+      clipboard
+    );
     return res.status(201).json({
       success: true,
       data: clipboard,
